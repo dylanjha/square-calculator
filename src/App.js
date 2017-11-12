2cbb4ff12687d535bd2357182dd6b89388cc24ed
Dylan Jhaveri <dylanjhaveri@gmail.com>
2017-11-12 09:30:31 -0800

    use react-ga to set up google analytics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import FeeCalculator from './components/FeeCalculator'
+import ReactGA from 'react-ga'
 import './App.css'
+
+console.info('REACT_APP_GA_ID', process.env.REACT_APP_GA_ID)
+if (process.env.REACT_APP_GA_ID) {
+  ReactGA.initialize(process.env.REACT_APP_GA_ID)
+  ReactGA.pageview(window.location.pathname + window.location.search)
+}
 
 function App () {
   return <FeeCalculator />
