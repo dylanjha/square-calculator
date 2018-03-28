fc551fe3bfeb35e11883577719614fbe690b4cdb
Dylan Jhaveri <dylanjhaveri@gmail.com>
2018-03-27 21:08:00 -0700

    hardcoded math calculations

--- src/components/Results.js
+++ src/components/Results.js
@@ -7,14 +7,63 @@ function Results ({ amount, country, paymentMethod }) {
   if (paymentMethod && amount) {
     totals = paymentMethod.getTotal(amount)
   }
+
+  // const leftW = '100px'
+
   return (
-    <div className='results'>
-      <div> { country } (country) </div>
-      <div>
-        <span>{totals.fee} </span><span className='fee'>fee</span>
+    <div className='results clearfix'>
+      <div className='results-summary'>
+        <div> { country } (country) </div>
+        <div>
+          <span>{totals.fee} </span><span className='fee'>fee</span>
+        </div>
+        <div>
+          <span>{totals.netAmount} </span><span className='net-amount'>amount after fee</span>
+        </div>
       </div>
-      <div>
-        <span>{totals.netAmount} </span><span className='net-amount'>amount after fee</span>
+      <div className='results-math'>
+        <div className='math-sale'>
+          <span className='math-left-col'>
+            sale
+          </span>
+          <span className='math-right-col'>
+            $10.00
+          </span>
+        </div>
+
+        <div className='math-fee'>
+          <div className='math-fee-percent'>
+            <span className='math-left-col math-fee-percent-label'>
+              10.00 * 0.025 =
+            </span>
+            <span className='math-right-col math-fee-percent-amount'>
+              - 0.25
+            </span>
+          </div>
+          <div className='math-after-percent-fee'>
+            <span className='math-left-col'>
+            </span>
+            <span className='math-right-col'>
+              $9.75
+            </span>
+          </div>
+          <div className='math-fee-flat'>
+            <span className='math-left-col'>
+              $0.10
+            </span>
+            <span className='math-right-col math-fee-flat-amount'>
+              - 0.10
+            </span>
+          </div>
+        </div>
+
+        <div className='math-after-fee'>
+          <span className='math-left-col'>
+          </span>
+          <span className='math-right-col'>
+            $9.65
+          </span>
+        </div>
       </div>
     </div>
   )
