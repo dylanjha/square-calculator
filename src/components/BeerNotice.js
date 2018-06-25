4111c36fb8370fc62f8dc336c43e1e8bcc5f711b
Dylan Jhaveri <dylanjhaveri@gmail.com>
2018-06-24 23:03:45 -0700

    add a11y for emoji and link to github

--- src/components/BeerNotice.js
+++ src/components/BeerNotice.js
@@ -3,7 +3,7 @@ import React from 'react'
 function DonateNotice () {
   return (
     <div className='alert alert-success' style={{marginTop: '30px'}}>
-      <span style={{fontSize: '20px'}}>🍺 </span>
+      <span role="img" aria-label="beer glass" style={{fontSize: '20px'}}>🍺 </span>
       <span>If you find this useful consider</span>
       {' '}
       <a href='https://cash.me/$dylanj' target='_blank' rel='noopener noreferrer'>
