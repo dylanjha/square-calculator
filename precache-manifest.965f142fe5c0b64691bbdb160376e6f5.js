94c1df67a0e926f9ad47d13b44f4bbcd3d5643c1
Dylan Jhaveri <dylanjhaveri@gmail.com>
2019-07-03 06:19:16 -0700

    Updates

---
+++ precache-manifest.965f142fe5c0b64691bbdb160376e6f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aee21a1ac1067a3b4b13188692db1d3a",
+    "url": "/square-calculator/index.html"
+  },
+  {
+    "revision": "bdd3700e6b890a4fe91e",
+    "url": "/square-calculator/static/css/main.794304eb.chunk.css"
+  },
+  {
+    "revision": "492261971b4255b7e4dd",
+    "url": "/square-calculator/static/js/2.a40b58b1.chunk.js"
+  },
+  {
+    "revision": "bdd3700e6b890a4fe91e",
+    "url": "/square-calculator/static/js/main.e66cb5d4.chunk.js"
+  },
+  {
+    "revision": "9a8d8424791afcd8c69a",
+    "url": "/square-calculator/static/js/runtime~main.bd04c827.js"
+  }
+]);
